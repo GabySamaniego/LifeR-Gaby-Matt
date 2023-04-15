4df54db699e6bcd1b9c738d4cc3fe12e5fac19a4
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2023-04-15 05:24:42 -0700

    Edit code and add 2023 bird list

--- lifeR locations.R
+++ lifeR locations.R
@@ -1,4 +1,4 @@
-# Trying lifeR for birdwatching
+# Trying lifeR for bird watching
 # Gaby and Matt
 # gaby@savehummingbirds,org
 # 2022-05-24
@@ -19,14 +19,37 @@ list_file <- "data/ebird_world_life_list.csv"
 user_list <- read.csv(file = list_file)
 
 # Extract the common names of species from your list
-my_species <- SplitNames(x = user_list$Species)$Common
+my_species <- user_list$Common.Name
 
 # Read in eBird API key from a text file; replace the argument to file with 
 # the actual location of your eBird key file
 key <- scan(file = "API.txt", what = "character")
 
 # A single center requires vector of coordinates
-locs <- c(35.171, -111.637)
+locs <- c(32.1, -110.7)
 SitesReport(centers = locs, 
             ebird_key = key, 
             species_seen = my_species)
+
+##### Year list 2023
+
+# Location of the file with your year list
+list_file_23 <- "data/ebird_world_year_2023_list.csv"
+
+# Read the list of species into memory
+user_list_23 <- read.csv(file = list_file_23)
+
+# Extract the common names of species from your list
+my_species_23 <- user_list$Common.Name
+
+# Read in eBird API key from a text file; replace the argument to file with 
+# the actual location of your eBird key file
+key <- scan(file = "API.txt", what = "character")
+
+# A single center requires vector of coordinates
+# Change these, unless you really want to go birding near McCall, Idaho
+locs_23 <- c(32.1, -110.7)
+SitesReport(centers = locs_23, 
+            ebird_key = key, 
+            species_seen = my_species_23)
+
